1c92e805b6e144014c82817e4d5dfdc0e0ea6daf
Zapratkyn <gilles.ponpon@gmail.com>
2025-07-17 22:37:34 +0200

    rm game files

--- game_classes/GameOver.cpp
+++
@@ -1,8 +0,0 @@
-#include "../game_include/GameOver.hpp"
-
-GameOver::GameOver(Loader *loader) : Object(loader)
-{
-	position = glm::vec3(-0.5f, 0.5f, 0.0f);
-	std::map<std::string, GLuint> vaos = loader->getVAOs();
-	VAO = vaos["game_over"];
-}
--- game_classes/Restart.cpp
+++
@@ -1,8 +0,0 @@
-#include "../game_include/Restart.hpp"
-
-Restart::Restart(Loader *loader) : Object(loader)
-{
-	position = glm::vec3(-0.1f, 0.12f, 0.0f);
-	std::map<std::string, GLuint> vaos = loader->getVAOs();
-	VAO = vaos["restart_button"];
-}
